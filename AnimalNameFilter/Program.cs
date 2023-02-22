2c4477e2506be5a809c9302ec083b3275e0928b2
lcosoriore <69127397+lcosoriore@users.noreply.github.com>
2023-02-22 12:13:28 -0500

    filters all green animals whose name starts with a vowel

---
+++ AnimalNameFilter/Program.cs
@@ -0,0 +1,16 @@
+﻿using AnimalNameFilter;
+
+Querys querys = new Querys();
+PrintValues(querys.AllCollection());
+PrintValues(querys.AnimalColorGreenAndVowel());
+
+// filters all green animals whose name starts with a vowel
+void PrintValues(IEnumerable<Animal> ListAnimal)
+{
+    Console.WriteLine("{0,-60} {1,15}\n", "Name", "Color");
+
+    foreach (var item in ListAnimal)
+    {
+        Console.WriteLine("{0,-60} {1,15}\n", item.Name, item.Color);
+    }
+}
---
+++ AnimalNameFilter/Querys.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AnimalNameFilter
+{
+    public class Querys
+    {
+        List<Animal> animales = new List<Animal>();
+        public Querys() {
+            animales.Add(new Animal() { Name = "Hormiga", Color = "Rojo" });
+            animales.Add(new Animal() { Name = "Lobo", Color = "Gris" });
+            animales.Add(new Animal() { Name = "Elefante", Color = "Gris" });
+            animales.Add(new Animal() { Name = "Pantegra", Color = "Negro" });
+            animales.Add(new Animal() { Name = "Gato", Color = "Negro" });
+            animales.Add(new Animal() { Name = "Iguana", Color = "Verde" });
+            animales.Add(new Animal() { Name = "Sapo", Color = "Verde" });
+            animales.Add(new Animal() { Name = "Camaleon", Color = "Verde" });
+            animales.Add(new Animal() { Name = "Gallina", Color = "Blanco" });
+        }
+
+        public IEnumerable<Animal> AllCollection()
+        {
+            return animales;
+        }
+
+        public IEnumerable<Animal> AnimalColorGreenAndVowel()
+        {
+            return animales.Where(p => p.Color.Contains("Verde") && (p.Name.StartsWith("A")|| p.Name.StartsWith("E")|| p.Name.StartsWith("I")|| p.Name.StartsWith("O")|| p.Name.StartsWith("U")));
+        }
+    }
+}
